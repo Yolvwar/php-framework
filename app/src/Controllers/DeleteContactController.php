eb886ff1b2944a354c7f6cd2a1c7918206b0eda2
Yoann Mallet <yoann.mallet@eemi.com>
2024-11-30 21:06:58 +0100

    Feat: add DeleteContactController to handle deleting a specific contact form

---
+++ app/src/Controllers/DeleteContactController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Http\Request;
+use App\Http\Response;
+use App\Entities\Contact;
+use App\Controllers\AbstractController;
+
+class DeleteContactController extends AbstractController
+{
+  public function process(Request $request): Response
+  {
+    $uri = $request->getUri();
+    $filename = basename($uri);;
+    return $this->deleteContact($filename);
+  }
+
+  private function deleteContact(string $filename): Response
+  {
+    $directory = __DIR__ . "/../../var/contacts/";
+
+    // Add .json extension if missing
+    if (!str_ends_with($filename, '.json')) {
+      $filename .= '.json';
+    }
+
+    $filePath = $directory . $filename;
+
+    if (!file_exists($filePath)) {
+      return $this->jsonResponse(['error' => 'Contact not found'], 404);
+    }
+
+    unlink($filePath);
+
+    return new Response('', 204);
+  }
+}
